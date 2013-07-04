78b5df265d4cf8538c0e9b668e436c15b72c480d
Lars Kiesow <lkiesow@uos.de>
2013-07-04 17:12:58 +0200

    xmp: xmp generation with dc data finished

--- modules/xmp/xmp/get_data.py
+++ modules/xmp/xmp/get_data.py
@@ -33,12 +33,12 @@ def get_request_data(username=None, password=None):
 	return url, auth
 
 
-def request_media(media_id, username=None, password=None):
+def request_media(id, username=None, password=None):
 
 	# Prepare request data
 	url, auth = get_request_data(username, password)
 
-	req  = urllib2.Request('%s/view/media/%s?with_name=true' % url)
+	req  = urllib2.Request('%s/view/media/%s?with_name=1' % (url,id))
 	if auth:
 		req.add_header(*auth)
 	req.add_header('Accept', 'application/json')
@@ -54,6 +54,7 @@ def request_media(media_id, username=None, password=None):
 def load_xmp( id, type='media' ):
 	'''This method will try to load the XMP for a given object from the
 	filesystem. If there is no file, a new XMP structure is created instead.
+	'''
 
 	xmpfilename = config.XMP_FILE_REPOSITORY + '/media_' + id
 
@@ -70,6 +71,45 @@ def load_xmp( id, type='media' ):
 
 	# Remove DC data from XMP as we get the new one from the core webservice
 	for elem in dc_elements:
-		xmp.delete_property( libxmp.consts.XMP_NS_DC, elem )
+		xmp.delete_property( XMP_NS_DC, elem )
 	
 	return xmp
+
+
+def include_lf_dc(lf_data, xmp):
+	'''Include elements from a given dataset (result from a lf:core webservice
+	request) into a given XMP structure.
+	'''
+	for data in lf_data:
+		lang = data.get('dc:language')
+		for key in ['title', 'description', 'rights']:
+			value = data.get('dc:' + key)
+			if value:
+				if lang:
+					xmp.set_localized_text(XMP_NS_DC, key, None, lang, value)
+				else:
+					xmp.set_property(XMP_NS_DC, key, value)
+
+		for key in ['coverage', 'identifier', 'type']:
+			value = data.get('dc:' + key)
+			if value:
+				xmp.set_property(XMP_NS_DC, key, value)
+
+		for key in ['date', 'language', 'relation', 'source']:
+			value = data.get('dc:' + key)
+			if value:
+				xmp.append_array_item(XMP_NS_DC, key, value)
+
+		# Add persons
+		# TODO: Add publisher (get name from core)
+		for key in ['contributor', 'creator']: #, 'publisher']:
+			for uid, name in data.get('dc:' + key).iteritems() or []:
+				xmp.append_array_item(XMP_NS_DC, key, name)
+
+		# Add subjects
+		for subj in data.get('dc:subject') or []:
+				xmp.append_array_item(XMP_NS_DC, 'subject', subj)
+
+			get_dc_array(     xmp, dc_prop, 'contributor' )
+			get_dc_array(     xmp, dc_prop, 'creator' )
+			get_dc_array(     xmp, dc_prop, 'publisher' )
