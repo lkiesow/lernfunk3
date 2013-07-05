6cba0dc1682157cbb7deca87f624529b247f0d6b
Lars Kiesow <lkiesow@uos.de>
2013-07-05 16:25:59 +0200

    xmp: get data is working (uncached)

--- modules/xmp/xmp/get_data.py
+++ modules/xmp/xmp/get_data.py
@@ -7,10 +7,19 @@
 	:copyright: 2013 by Lars Kiesow
 	:license: FreeBSD, see LICENSE for more details.
 """
+
+# Set default encoding to UTF-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 import urllib2
 import libxmp
 from libxmp.consts import XMP_NS_DC
+import json
 import os.path
+from base64 import urlsafe_b64encode
+import config
 
 
 dc_elements = ['contributor', 'coverage', 'creator', 'date', 'description',
@@ -20,11 +29,14 @@ dc_elements = ['contributor', 'coverage', 'creator', 'date', 'description',
 
 def get_request_data(username=None, password=None):
 
+	path = config.LERNFUNK_CORE_PATH \
+			if config.LERNFUNK_CORE_PATH.endswith('/') \
+			else config.LERNFUNK_CORE_PATH + '/'
 	url = '%s://%s:%i%s' % (
 			config.LERNFUNK_CORE_PROTOCOL,
 			config.LERNFUNK_CORE_HOST,
 			config.LERNFUNK_CORE_PORT,
-			config.LERNFUNK_CORE_PATH )
+			path )
 	auth = ('Authorization', 'Basic ' + urlsafe_b64encode("%s:%s" % \
 			( username, password ))) \
 			if username and password \
@@ -33,12 +45,13 @@ def get_request_data(username=None, password=None):
 	return url, auth
 
 
-def request_media(id, username=None, password=None):
-
+def request_lf_data(type, id, username=None, password=None):
+	'''Request media with a given identifier from the lf core server.
+	'''
 	# Prepare request data
 	url, auth = get_request_data(username, password)
 
-	req  = urllib2.Request('%s/view/media/%s?with_name=1' % (url,id))
+	req  = urllib2.Request('%sview/%s/%s?with_name=1' % (url,type,id))
 	if auth:
 		req.add_header(*auth)
 	req.add_header('Accept', 'application/json')
@@ -48,15 +61,15 @@ def request_media(id, username=None, password=None):
 	finally:
 		u.close()
 
-	return media['result']['lf:media']:
+	return media['result']['lf:' + type]
 
 
-def load_xmp( id, type='media' ):
+def load_xmp( type, id ):
 	'''This method will try to load the XMP for a given object from the
 	filesystem. If there is no file, a new XMP structure is created instead.
 	'''
 
-	xmpfilename = config.XMP_FILE_REPOSITORY + '/media_' + id
+	xmpfilename = '%s/%s_%s.xmp' % (config.XMP_FILE_REPOSITORY, type, id)
 
 	if not os.path.isfile(xmpfilename):
 		return libxmp.core.XMPMeta()
@@ -86,7 +99,7 @@ def include_lf_dc(lf_data, xmp):
 			value = data.get('dc:' + key)
 			if value:
 				if lang:
-					xmp.set_localized_text(XMP_NS_DC, key, None, lang, value)
+					xmp.set_localized_text(XMP_NS_DC, key, None, lang, value.encode('utf-8'))
 				else:
 					xmp.set_property(XMP_NS_DC, key, value)
 
@@ -103,9 +116,32 @@ def include_lf_dc(lf_data, xmp):
 		# Add persons
 		# TODO: Add publisher (get name from core)
 		for key in ['contributor', 'creator']: #, 'publisher']:
-			for uid, name in data.get('dc:' + key).iteritems() or []:
+			for uid, name in (data.get('dc:' + key) or {}).iteritems():
 				xmp.append_array_item(XMP_NS_DC, key, name)
 
 		# Add subjects
 		for subj in data.get('dc:subject') or []:
 				xmp.append_array_item(XMP_NS_DC, 'subject', subj)
+
+	return xmp
+
+
+def get_xmp(type, id, user=None, password=None):
+	'''Get/Generate an XMP structure for a specified object from the Lernfunk db.
+	'''
+	# Get necessary data
+	lf_data = request_lf_data(type, id, user, password)
+	xmp     = load_xmp(type, id)
+
+	# Enrich XMP
+	include_lf_dc(lf_data, xmp)
+
+	# Generate string
+	return xmp.serialize_to_str( 
+		padding=0,
+		omit_packet_wrapper=True,
+		use_compact_format=True)
+
+
+if __name__ == '__main__':
+	print get_xmp('media', 'e8ad5959-9d26-11e2-a381-047d7b0f869a', 'lkiesow', 'test')
